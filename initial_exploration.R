d791518d60f103e92dba4a48c702d11746bf44e1
stedy <zstednic@fhcrc.org>
2015-05-06 22:54:09 -0700

    graph by weekday

--- initial_exploration.R
+++ initial_exploration.R
@@ -6,10 +6,10 @@ library(ggplot2)
 raw <- read.csv("data/PDR-15-1195.csv", colClasses="character")
 raw$scheduled_departure_dt <- as.POSIXlt(raw$scheduled_departure)
 raw$actual_departure <- as.POSIXlt(raw$actual_departure)
-raw$difference <- as.numeric(raw$actual_departure - raw$scheduled_departure_dt) / 60
+raw$difference <- abs(as.numeric(raw$actual_departure - raw$scheduled_departure_dt) / 60)
 raw$scheduled_departure <- as.Date(raw$scheduled_departure)
 
-raw <- subset(raw, difference >= 0 & difference <= 5000)
+#raw <- subset(raw, difference >= 0 & difference <= 5000)
 ggplot2::qplot(raw$difference)
 
 holidays <- as.Date(c("2014-01-01", "2014-05-26",
@@ -24,3 +24,14 @@ regular.days <-
 
 ggplot(regular.days, aes(scheduled_departure, difference, group=route_name, color=route_name)) +
   geom_path(alpha = 0.9)
+
+seattle.bi <- subset(regular.days, route_name == "Seattle - Bainbridge Island")
+seattle.bi$weekday <- weekdays(seattle.bi$scheduled_departure)
+
+ggplot(seattle.bi, aes(scheduled_departure, difference, group=weekday, color=weekday)) +
+  geom_path(alpha = 0.9)
+
+#write as javascript date for mapping
+seattle.bi$jdate <- str((seattle.bi$scheduled_departure - as.Date("1970-01-01")) * 60 * 60 * 24)
+write.table(seattle.bi[c('jdate', 'difference')],  "afile.JSON", row.names=F,
+            sep=":", quote=T)
